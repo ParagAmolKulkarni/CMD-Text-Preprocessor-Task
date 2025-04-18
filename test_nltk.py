62ee1ed4c6876e6603afdaa6e240ec885b125836
ParagAmolKulkarni <paragkulkarni007@proton.me>
2025-04-18 20:23:20 +0530

    Delete test_nltk.py

--- test_nltk.py
+++
@@ -1,4 +0,0 @@
-import nltk
-print("NLTK Version:", nltk.__version__)
-print("Punkt available:", nltk.data.find('tokenizers/punkt'))
-print("Stopwords available:", nltk.data.find('corpora/stopwords'))

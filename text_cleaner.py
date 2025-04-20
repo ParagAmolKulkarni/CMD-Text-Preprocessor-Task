e3cc25036ce99d968403968b2e795afcc2968ff4
ParagAmolKulkarni <paragkulkarni007@proton.me>
2025-04-20 19:59:14 +0530

    Add files via upload

---
+++ text_cleaner.py
@@ -0,0 +1,36 @@
+import re
+import nltk
+import os
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+
+nltk_data_path = os.path.join(os.path.expanduser("~"), "nltk_data")
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
+
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir=nltk_data_path)
+
+try:
+    nltk.data.find('tokenizers/punkt_tab')
+except LookupError:
+    nltk.download('punkt_tab', download_dir=nltk_data_path)
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords', download_dir=nltk_data_path)
+
+class TextCleaner:
+    def __init__(self):
+        self.stop_words = set(stopwords.words('english'))
+    
+    def clean_text(self, text):
+        text = text.lower()
+        text = re.sub(r'[^a-z\s]', '', text)
+        tokens = word_tokenize(text)
+        return [word for word in tokens if word not in self.stop_words]
